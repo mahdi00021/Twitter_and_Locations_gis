cac409d3aba61f165c6832f7eac0f383b943a28b
mahdi norouzi <m73hdi@gmail.com>
2020-12-04 20:22:02 +0330

    Add files via upload

---
+++ MainPy/insertgeommetrydjango.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.location_finder_locationm(latitude, longitude, name_point,the_geom )
+	VALUES ( 34.4522185, 5.8007813, 'europa',ST_SetSRID(ST_MakePoint(5.8007813,34.4522185),4326));
+INSERT INTO public.location_finder_locationm(latitude, longitude, name_point,the_geom)
+	VALUES ( 34.4522103, 5.8009913, 'paris',ST_SetSRID(ST_MakePoint(5.8009913,34.4522103),4326));
+INSERT INTO public.location_finder_locationm(latitude, longitude, name_point,the_geom)
+	VALUES ( 34.5511153, 5.8259913, 'rom',ST_SetSRID(ST_MakePoint(5.8259913,34.5511153),4326));
+INSERT INTO public.location_finder_locationm(latitude, longitude, name_point,the_geom)
+	VALUES ( 34.6679052, 5.7058935, 'stackholm',ST_SetSRID(ST_MakePoint(5.7058935,34.6679052),4326));	
